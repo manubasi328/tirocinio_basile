08c5a94f9750a6abf3f4300fabd6d59a67c50962
Manuel Basile <mbasile@hnode01.hpc.prv>
2024-11-28 10:38:38 +0100

    kge e nations

---
+++ kge/scriptlp4kgtorchpar.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d50b73c5d6992a06d61954d89d52cbbb10761720c0218327800caf9d7d4d322e
+size 689
---
+++ nations/scriptlp4kgtorchpar.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:80c24732461344a75d95cd00f3c730f56c1c09b54a26f936dc3df4d2dd56f725
+size 689
