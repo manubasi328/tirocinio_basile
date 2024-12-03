fe0873afc446905c61a0b01befc13f1efcfc690d
Manuel Basile <mbasile@hnode01.hpc.prv>
2024-12-03 16:49:12 +0100

    wn18rr

---
+++ wn18rr/compute_stats.pl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0d1cdb27a27362b1efba0e9ae17b80930ce2499241291005f1c0557b4b21c5ea
+size 307
---
+++ wn18rr/scriptlp4kg_stats.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c95fd57c46e810f431caaf0e2a8757daf494abfb5133645e2a6074cefe67e806
+size 573
