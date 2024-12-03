1a4b169e8c0681124100ecabf6cd102449f98436
Manuel Basile <mbasile@hnode01.hpc.prv>
2024-12-03 16:37:43 +0100

    nell

---
+++ nell/compute_par.pl
@@ -0,0 +1,16 @@
+:- use_module(library(liftcover)).
+
+:- [train].
+
+:-lift.
+
+:- set_lift(verbosity,5).
+:- set_lift(parameter_learning,em_torch).
+:- set_lift(processor,gpu).
+:- set_lift(threads,2).
+
+:- initialization(main,main).
+main:-
+  read_rules_anyburl('learn/anyburl-rules.txt', R),
+  compute_par_pos_kg(R,'stats.pl',R1),
+  write_rules_anyburl(R1,'out_lp4kg_torch_lf.txt').
---
+++ nell/train.pl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cbfdd946b1097b92a50ee8ff8c9ac2bff33e7a5c2cfe665aa6b2ccee9fb93e43
+size 5395708
