89193aa19812d8e9cd0e827e8bb3b93e36b76bf5
Manuel Basile <mbasile@hnode01.hpc.prv>
2024-12-31 16:34:23 +0100

    aggiunta script prove

---
+++ wn18rr/compute_par_l1g5.pl
@@ -0,0 +1,18 @@
+:- use_module(library(liftcover)).
+
+:- [train].
+
+:-lift.
+
+:- set_lift(verbosity,5).
+:- set_lift(parameter_learning,em_torch).
+:- set_lift(processor,gpu).
+:- set_lift(threads,2).
+:- set_lift(regularization,l1).
+:- set_lift(gamma,5).
+
+:- initialization(main,main).
+main:-
+  read_rules_anyburl('learn/anyburl-rules.txt', R),
+  compute_par_kg(R,'stats.pl',R1),
+  write_rules_anyburl(R1,'out_lp4kg_torch_lf_l1g5.txt').
